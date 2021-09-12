3d3eec8ff44b7eb515cb9c6a6991f888276d2d34
Tim Primmer <timprimmer@gmail.com>
2021-09-12 12:10:27 -0500

    added thought and reaction models

---
+++ models/Thought.js
@@ -0,0 +1,71 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent _id
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: 'You need to provide some text!',
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'You need to provide some thought text!',
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+      type: String,
+      required: 'You need to provide a username!',
+    },
+    reactions: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+// get total count of 'friends' on retrieval
+ThoughtSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// create the Thought model using the UserSchema
+const Thought = model('Thought', ThoughtSchema);
+
+// export the User model
+module.exports = Thought;
