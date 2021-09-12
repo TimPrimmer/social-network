aa52d40d555a6c0742e021832cb2d4ab7b01573f
Tim Primmer <timprimmer@gmail.com>
2021-09-12 11:48:43 -0500

    added user model

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: 'You need to provide a username!',
+      trim: true
+    },
+    username: {
+      type: String,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "You need to provide a valid email!"
+      },
+      required: [true, "Email required"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+// get total count of 'friends' on retrieval
+UserSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
